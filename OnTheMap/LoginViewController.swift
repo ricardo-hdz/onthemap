bf4feaee02841cb162252ec80577042dc9418860
Ricardo Hernandez <ricardo.hdz@gmail.com>
2015-09-01 20:51:15 -0700

    Scaffold

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Ricardo Hdz on 9/1/15.
+//  Copyright (c) 2015 Ricardo Hdz. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class LoginViewController: UIViewController {
+
+
+}
---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Ricardo Hdz on 9/1/15.
+//  Copyright (c) 2015 Ricardo Hdz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {}
